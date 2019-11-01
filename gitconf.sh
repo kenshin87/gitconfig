ccbaea3b75094047e515df8c8c409852f90e3d0e
Liu Zhenfeng <lkenshin@hotmail.com>
2019-11-01 16:22:07 +0800

    delete redundant lines

--- gitconf.sh
+++ gitconf.sh
@@ -5,9 +5,7 @@ git config --global alias.st status
 git config --global alias.cm "commit -m"
 
 
-git commit -m "add timeutil"
 git config --global user.email "lkenshin@hotmail.com"
 git config --global user.name "Liu Zhenfeng"
 git config --global credential.helper cache
-git commit -m "add timeutil"
 
